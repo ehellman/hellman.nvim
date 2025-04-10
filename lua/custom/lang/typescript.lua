9b9495babdaeea2ae789791db2c0b733df414b17
Erik Hellman <iam@erikhellman.com>
2025-04-10 10:23:33 +0200

    chore(typescript): replace ts_ls with vtsls

--- lua/custom/lang/typescript.lua
+++ lua/custom/lang/typescript.lua
@@ -4,7 +4,9 @@ return {
     "WhoIsSethDaniel/mason-tool-installer.nvim",
     opts = {
       ensure_installed = {
-        "ts_ls",
+        -- "ts_ls",
+        "vtsls",
+        "prettier",
         "prettierd",
       },
     },
@@ -22,9 +24,9 @@ return {
     "neovim/nvim-lspconfig",
     opts = {
       servers = {
-        ---@type vim.lsp.Config
+        ---@type vim.lsp.ClientConfig
         ts_ls = {
-          enabled = true,
+          enabled = false,
           settings = {
             typescript = {
               inlayHints = {
@@ -39,20 +41,161 @@ return {
             },
           },
         },
-        -- vtsls = {
-        --   enabled = false,
-        -- },
+        ---@type vim.lsp.ClientConfig
+        vtsls = {
+          enabled = true,
+          -- explicitly add default filetypes, so that we can extend
+          -- them in related extras
+          filetypes = {
+            "javascript",
+            "javascriptreact",
+            "javascript.jsx",
+            "typescript",
+            "typescriptreact",
+            "typescript.tsx",
+          },
+          settings = {
+            complete_function_calls = true,
+            vtsls = {
+              enableMoveToFileCodeAction = true,
+              autoUseWorkspaceTsdk = true,
+              experimental = {
+                maxInlayHintLength = 30,
+                completion = {
+                  enableServerSideFuzzyMatch = true,
+                },
+              },
+            },
+            typescript = {
+              updateImportsOnFileMove = { enabled = "always" },
+              suggest = {
+                completeFunctionCalls = true,
+              },
+              inlayHints = {
+                enumMemberValues = { enabled = true },
+                functionLikeReturnTypes = { enabled = true },
+                parameterNames = { enabled = "literals" },
+                parameterTypes = { enabled = true },
+                propertyDeclarationTypes = { enabled = true },
+                variableTypes = { enabled = false },
+              },
+            },
+          },
+          keys = {
+            {
+              "gD",
+              function()
+                local params = vim.lsp.util.make_position_params()
+                HellVim.lsp.execute({
+                  command = "typescript.goToSourceDefinition",
+                  arguments = { params.textDocument.uri, params.position },
+                  open = true,
+                })
+              end,
+              desc = "goto source [D]efinition",
+            },
+            {
+              "gR",
+              function()
+                HellVim.lsp.execute({
+                  command = "typescript.findAllFileReferences",
+                  arguments = { vim.uri_from_bufnr(0) },
+                  open = true,
+                })
+              end,
+              desc = "file [R]eferences",
+            },
+            {
+              "<leader>co",
+              HellVim.lsp.action["source.organizeImports"],
+              desc = "[o]rganize imports",
+            },
+            {
+              "<leader>cM",
+              HellVim.lsp.action["source.addMissingImports.ts"],
+              desc = "add [M]issing imports",
+            },
+            {
+              "<leader>cu",
+              HellVim.lsp.action["source.removeUnused.ts"],
+              desc = "remove [u]nused imports",
+            },
+            {
+              "<leader>cx",
+              HellVim.lsp.action["source.fixAll.ts"],
+              desc = "fi[x] all diagnostics",
+            },
+            {
+              "<leader>cV",
+              function()
+                HellVim.lsp.execute({ command = "typescript.selectTypeScriptVersion" })
+              end,
+              desc = "select TS [V]ersion",
+            },
+          },
+        },
       },
-      -- setup = {
-      --
-      -- ts_ls = function()
-      --
-      --   return true
-      -- end,
-      -- vtsls = function()
-      --   return true
-      -- end,
-      -- }
+      setup = {
+        ts_ls = function()
+          -- disable tsserver
+          return true
+        end,
+        vtsls = function(_, opts)
+          HellVim.lsp.on_attach(function(client, buffer)
+            client.commands["_typescript.moveToFileRefactoring"] = function(command, ctx)
+              ---@type string, string, lsp.Range
+              local action, uri, range = unpack(command.arguments)
+
+              local function move(newf)
+                client.request("workspace/executeCommand", {
+                  command = command.command,
+                  arguments = { action, uri, range, newf },
+                })
+              end
+
+              local fname = vim.uri_to_fname(uri)
+              client.request("workspace/executeCommand", {
+                command = "typescript.tsserverRequest",
+                arguments = {
+                  "getMoveToRefactoringFileSuggestions",
+                  {
+                    file = fname,
+                    startLine = range.start.line + 1,
+                    startOffset = range.start.character + 1,
+                    endLine = range["end"].line + 1,
+                    endOffset = range["end"].character + 1,
+                  },
+                },
+              }, function(_, result)
+                ---@type string[]
+                local files = result.body.files
+                table.insert(files, 1, "Enter new path...")
+                vim.ui.select(files, {
+                  prompt = "Select move destination:",
+                  format_item = function(f)
+                    return vim.fn.fnamemodify(f, ":~:.")
+                  end,
+                }, function(f)
+                  if f and f:find("^Enter new path") then
+                    vim.ui.input({
+                      prompt = "Enter move destination:",
+                      default = vim.fn.fnamemodify(fname, ":h") .. "/",
+                      completion = "file",
+                    }, function(newf)
+                      return newf and move(newf)
+                    end)
+                  elseif f then
+                    move(f)
+                  end
+                end)
+              end)
+            end
+          end, "vtsls")
+          -- copy typescript settings to javascript
+          opts.settings.javascript =
+            vim.tbl_deep_extend("force", {}, opts.settings.typescript, opts.settings.javascript or {})
+        end,
+      },
     },
   },
   {
@@ -69,71 +212,72 @@ return {
       },
     },
   },
-  -- {
-  --   'vuki656/package-info.nvim',
-  --   -- event = 'VeryLazy',
-  --   -- event = 'BufRead package.json',
-  --   -- event = 'BufReadPost package.json',
-  --   config = function()
-  --     require('package-info').setup()
-  --   end,
-  --   -- config = function()
-  --   --   local package_info = require('package-info')
-  --   --   local opts = {
-  --   --   }
-  --   --   package_info.setup(opts)
-  --   --   -- local c = require('package-info/utils/constants')
-  --   --   -- vim.api.nvim_create_autocmd('User', {
-  --   --   --   group = c.AUTOGROUP,
-  --   --   --   pattern = c.LOAD_EVENT,
-  --   --   --   callback = function()
-  --   --   --     -- execute a groupless autocmd so heirline can update
-  --   --   --     vim.cmd.doautocmd('User', 'DkoPackageInfoStatusUpdate')
-  --   --   --   end,
-  --   --   -- })
-  --   -- end,
-  --   keys = {
-  --     {
-  --       '<leader>pnt',
-  --       "<cmd>lua require('package-info').get_status()<CR>",
-  --       desc = '[g]et status',
-  --       silent = true,
-  --       noremap = true,
-  --     },
-  --     {
-  --       '<leader>pns',
-  --       "<cmd>lua require('package-info').show()<CR>",
-  --       desc = '[S]how',
-  --       silent = true,
-  --       noremap = true,
-  --     },
-  --     {
-  --       '<leader>pnh',
-  --       "<cmd>lua require('package-info').hide()<CR>",
-  --       desc = '[H]ide',
-  --       silent = true,
-  --       noremap = true,
-  --     },
-  --     {
-  --       '<leader>pnt',
-  --       "<cmd>lua require('package-info').toggle()<CR>",
-  --       desc = '[T]oggle',
-  --       silent = true,
-  --       noremap = true,
-  --     },
-  --     -- {
-  --     --   '<leader>pnh',
-  --     --   require('package-info').hide,
-  --     --   mode = 'n',
-  --     --   desc = '[H]ide',
-  --     -- silent = true, noremap = true },
-  --     {
-  --       '<leader>pnc',
-  --       "<cmd>lua require('package-info').change_version()<CR>",
-  --       desc = '[C]hange Package Version',
-  --       silent = true,
-  --       noremap = true,
-  --     },
-  --   },
-  -- },
+  {
+    "vuki656/package-info.nvim",
+    enabled = false,
+    -- event = 'VeryLazy',
+    -- event = 'BufRead package.json',
+    -- event = 'BufReadPost package.json',
+    config = function()
+      require("package-info").setup()
+    end,
+    -- config = function()
+    --   local package_info = require('package-info')
+    --   local opts = {
+    --   }
+    --   package_info.setup(opts)
+    --   -- local c = require('package-info/utils/constants')
+    --   -- vim.api.nvim_create_autocmd('User', {
+    --   --   group = c.AUTOGROUP,
+    --   --   pattern = c.LOAD_EVENT,
+    --   --   callback = function()
+    --   --     -- execute a groupless autocmd so heirline can update
+    --   --     vim.cmd.doautocmd('User', 'DkoPackageInfoStatusUpdate')
+    --   --   end,
+    --   -- })
+    -- end,
+    keys = {
+      {
+        "<leader>pnt",
+        "<cmd>lua require('package-info').get_status()<CR>",
+        desc = "[g]et status",
+        silent = true,
+        noremap = true,
+      },
+      {
+        "<leader>pns",
+        "<cmd>lua require('package-info').show()<CR>",
+        desc = "[S]how",
+        silent = true,
+        noremap = true,
+      },
+      {
+        "<leader>pnh",
+        "<cmd>lua require('package-info').hide()<CR>",
+        desc = "[H]ide",
+        silent = true,
+        noremap = true,
+      },
+      {
+        "<leader>pnt",
+        "<cmd>lua require('package-info').toggle()<CR>",
+        desc = "[T]oggle",
+        silent = true,
+        noremap = true,
+      },
+      -- {
+      --   '<leader>pnh',
+      --   require('package-info').hide,
+      --   mode = 'n',
+      --   desc = '[H]ide',
+      -- silent = true, noremap = true },
+      {
+        "<leader>pnc",
+        "<cmd>lua require('package-info').change_version()<CR>",
+        desc = "[C]hange Package Version",
+        silent = true,
+        noremap = true,
+      },
+    },
+  },
 }
