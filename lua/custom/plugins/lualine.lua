387d0172c0fb177dfcb95db10e79c9e29b5f55a4
Erik Hellman <iam@erikhellman.com>
2025-04-21 12:15:50 +0200

    chore(lualine): hide lualine on start

--- lua/custom/plugins/lualine.lua
+++ lua/custom/plugins/lualine.lua
@@ -7,6 +7,16 @@ return {
     "nvim-tree/nvim-web-devicons",
   },
   opts = {},
+  init = function()
+    vim.g.lualine_laststatus = vim.o.laststatus
+    if vim.fn.argc(-1) > 0 then
+      -- set an empty statusline till lualine loads
+      vim.o.statusline = " "
+    else
+      -- hide the statusline on the starter page
+      vim.o.laststatus = 0
+    end
+  end,
   config = function(_, opts)
     local lualine = require("lualine")
     local lualine_aug = vim.api.nvim_create_augroup("StatusLineWatchRecording", { clear = true })
