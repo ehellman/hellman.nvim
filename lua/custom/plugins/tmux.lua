9f76894e09254c8ad8a173b4fc288551839d803b
Erik Hellman <iam@erikhellman.com>
2025-04-21 14:25:14 +0200

    chore(tmux): move tmux plugins to single file

--- lua/custom/plugins/tmux.lua
+++ lua/custom/plugins/tmux.lua
@@ -1,21 +1,44 @@
 ---@type LazySpec
 return {
-  "aserowy/tmux.nvim",
-  event = "VeryLazy",
-  config = function()
-    local tmux = require("tmux")
-    tmux.setup({
-      copy_sync = {
-        enable = true, -- sync clipboard through tmux
-      },
-      navigation = {
-        cycle_navigation = false,
-        enable_default_keybindings = false,
-        persist_zoom = true,
-      },
-      resize = {
-        enable_default_keybindings = false,
-      },
-    })
-  end,
+  {
+    -- only used for clipboard
+    "aserowy/tmux.nvim",
+    event = "VeryLazy",
+    enabled = HellVim.is_tmux_term(),
+    config = function()
+      local tmux = require("tmux")
+      tmux.setup({
+        copy_sync = {
+          enable = true, -- sync clipboard through tmux
+        },
+        navigation = {
+          cycle_navigation = false,
+          enable_default_keybindings = false,
+          persist_zoom = true,
+        },
+        resize = {
+          enable_default_keybindings = false,
+        },
+      })
+    end,
+  },
+  {
+    -- navigation between tmux and nvim
+    "alexghergh/nvim-tmux-navigation",
+    event = "VeryLazy",
+    enabled = HellVim.is_tmux_term(),
+    keys = {
+      -- stylua: ignore start
+      { "<C-h>", "<Cmd>NvimTmuxNavigateLeft<CR>", desc = "navigate left" },
+      { "<C-j>", "<Cmd>NvimTmuxNavigateDown<CR>", desc = "navigate down" },
+      { "<C-k>", "<Cmd>NvimTmuxNavigateUp<CR>", desc = "navigate up" },
+      { "<C-l>", "<Cmd>NvimTmuxNavigateRight<CR>", desc = "navigate right" },
+      -- stylua: ignore end
+    },
+    config = function()
+      require("nvim-tmux-navigation").setup({
+        disable_when_zoomed = true, -- defaults to false
+      })
+    end,
+  },
 }
