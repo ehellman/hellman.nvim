950694dfd1fcd89271df97333f5770b08e6f1524
Erik Hellman <iam@erikhellman.com>
2025-08-15 16:29:24 +0200

    chore: update roslyn configuration

--- lua/custom/lang/csharp.lua
+++ lua/custom/lang/csharp.lua
@@ -23,6 +23,7 @@ end
 return {
   {
     "seblyng/roslyn.nvim",
+    -- version = "8dc729a",
     ft = { "cs" },
     dependencies = {
       {
@@ -42,45 +43,39 @@ return {
       },
     },
     ---@type RoslynNvimConfig
-    opts = {
-      --- disable missing-fields to mask "cmd" missing
-      ---@diagnostic disable-next-line: missing-fields
-      config = {
-        settings = {
-          ["csharp|inlay_hints"] = {
-            csharp_enable_inlay_hints_for_implicit_object_creation = true,
-            csharp_enable_inlay_hints_for_implicit_variable_types = true,
-            csharp_enable_inlay_hints_for_lambda_parameter_types = true,
-            csharp_enable_inlay_hints_for_types = true,
-            dotnet_enable_inlay_hints_for_indexer_parameters = true,
-            dotnet_enable_inlay_hints_for_literal_parameters = true,
-            dotnet_enable_inlay_hints_for_object_creation_parameters = true,
-            dotnet_enable_inlay_hints_for_other_parameters = true,
-            dotnet_enable_inlay_hints_for_parameters = true,
-            dotnet_suppress_inlay_hints_for_parameters_that_differ_only_by_suffix = true,
-            dotnet_suppress_inlay_hints_for_parameters_that_match_argument_name = true,
-            dotnet_suppress_inlay_hints_for_parameters_that_match_method_intent = true,
-          },
-          ["csharp|code_lens"] = {
-            dotnet_enable_references_code_lens = true,
-          },
-          ["csharp|symbol_search"] = {
-            dotnet_search_reference_assemblies = true,
-          },
-          ["csharp|formatting"] = {
-            dotnet_organize_imports_on_format = true,
-          },
-          ["csharp|completions"] = {
-            dotnet_provide_regex_completions = true,
-            dotnet_show_completion_items_from_unimported_namespaces = true,
-            dotnet_show_name_completion_suggestions = true,
-          },
+    --- disable missing-fields to mask "cmd" missing
+    ---@diagnostic disable-next-line: missing-fields
+    config = {
+      settings = {
+        ["csharp|inlay_hints"] = {
+          csharp_enable_inlay_hints_for_implicit_object_creation = true,
+          csharp_enable_inlay_hints_for_implicit_variable_types = true,
+          csharp_enable_inlay_hints_for_lambda_parameter_types = true,
+          csharp_enable_inlay_hints_for_types = true,
+          dotnet_enable_inlay_hints_for_indexer_parameters = true,
+          dotnet_enable_inlay_hints_for_literal_parameters = true,
+          dotnet_enable_inlay_hints_for_object_creation_parameters = true,
+          dotnet_enable_inlay_hints_for_other_parameters = true,
+          dotnet_enable_inlay_hints_for_parameters = true,
+          dotnet_suppress_inlay_hints_for_parameters_that_differ_only_by_suffix = true,
+          dotnet_suppress_inlay_hints_for_parameters_that_match_argument_name = true,
+          dotnet_suppress_inlay_hints_for_parameters_that_match_method_intent = true,
+        },
+        ["csharp|code_lens"] = {
+          dotnet_enable_references_code_lens = true,
+        },
+        ["csharp|symbol_search"] = {
+          dotnet_search_reference_assemblies = true,
+        },
+        ["csharp|formatting"] = {
+          dotnet_organize_imports_on_format = true,
+        },
+        ["csharp|completions"] = {
+          dotnet_provide_regex_completions = true,
+          dotnet_show_completion_items_from_unimported_namespaces = true,
+          dotnet_show_name_completion_suggestions = true,
         },
       },
     },
-    config = function(_, opts)
-      local roslyn = require("roslyn")
-      roslyn.setup(opts)
-    end,
   },
 }
